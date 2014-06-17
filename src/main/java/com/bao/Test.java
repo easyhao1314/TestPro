21e56277cd825f218e6159ddaf4022dbdd1d1c3a
baoguohao <baoguohao@sursen.net>
2014-06-17 21:41:41 +0800

    项目初始化

---
+++ src/main/java/com/bao/Test.java
@@ -0,0 +1,13 @@
+package com.bao;
+
+/**
+ * Created by baoguohao on 14-6-17.
+ */
+public class Test {
+    public static void main(String [] args){
+        System.out.println("测试github");
+    }
+
+
+
+}
